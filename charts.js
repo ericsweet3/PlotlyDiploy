1c429d0ac91146f01953a288ecf1b3788615914f
Eric Sweet <ericsweet677@gmail.com>
2021-03-27 11:13:45 -0500

    added missed steps

--- charts.js
+++ charts.js
@@ -3,7 +3,7 @@ function init() {
   var selector = d3.select("#selDataset");
 
   // Use the list of sample names to populate the select options
-  d3.json("/PlotlyDiploy/samples.json").then((data) => {
+  d3.json("samples.json").then((data) => {
     var sampleNames = data.names;
 
     sampleNames.forEach((sample) => {
@@ -32,7 +32,7 @@ function optionChanged(newSample) {
 
 // Demographics Panel 
 function buildMetadata(sample) {
-  d3.json("/PlotlyDiploy/samples.json").then((data) => {
+  d3.json("samples.json").then((data) => {
     var metadata = data.metadata;
     // Filter the data for the object with the desired sample number
     var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
@@ -56,7 +56,7 @@ function buildMetadata(sample) {
 // 1. Create the buildCharts function.
 function buildCharts(sample) {
   // 2. Use d3.json to load and retrieve the samples.json file 
-  d3.json("/PlotlyDiploy/samples.json").then((data) => {
+  d3.json("samples.json").then((data) => {
     // 3. Create a variable that holds the samples array. 
     var samples = data.samples;
 
@@ -67,14 +67,17 @@ function buildCharts(sample) {
     var FirstMetadata = metadataArray[0];
     //  5. Create a variable that holds the first sample in the array.
     var sampleFirst = samplesArray[0];
+    // 2. Create a variable that holds the first sample in the metadata array.
+    var firstSample = washingFreq[0];
 
     // 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
     var otuids = sampleFirst.otu_ids;
     var otulabels = sampleFirst.otu_labels;
     var samplevalues = sampleFirst.sample_values;
 
-    ;
-    console.log(wfreq);
+    // 3. Create a variable that holds the washing frequency.
+    var wfreqs = firstSample.wfreq
+
     // 7. Create the yticks for the bar chart.
     // Hint: Get the the top 10 otu_ids and map them in descending order  
     //  so the otu_ids with the most bacteria are last. 
@@ -123,6 +126,8 @@ function buildCharts(sample) {
 
     // 3. Use Plotly to plot the data with the layout.
     Plotly.newPlot("bubble", bubbleData, bubbleLayout);
+
+
 
     // 4. Create the trace for the gauge chart.
     var gaugeTrace = {
